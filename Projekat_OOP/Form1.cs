58064053302c4c4bfe2ed3e75e7b763935bca811
Danilo Ranđelović <danilo24randjelovic@gmail.com>
2022-11-17 10:59:20 +0100

    Add project files.

---
+++ Projekat_OOP/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekat_OOP
+{
+    public partial class frmIgra : Form
+    {
+        public frmIgra()
+        {
+            InitializeComponent();
+            for (int y = 0; y < matrica.GetLength(1); y++)
+            {
+                for (int x = 0; x < matrica.GetLength(0); x++)
+                {
+                    matrica[x, y] = new Polje();
+                }
+            }
+        }
+        Polje[,] matrica = new Polje[30, 20];
+        bool igra_gotova = true;
+        int broj_mina = 40;
+        int broj_neotvorenih_polja = 30 * 20;//kada broj neotvorenih polja bude jednak broju mina, igra je gotova-pobeda
+    }
+}
